af90d15af3ccc402b3a5d1cc88fcefc1991505d4
amreldeeb <amreldeeb641@gmail.com>
2025-12-13 00:00:20 +0200

    add Ktor implementation for CoinsRemoteDataSource with API calls for coin details and price history

--- composeApp/src/commonMain/kotlin/com/example/cryptowallet/app/coins/domain/CoinsRemoteDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/example/cryptowallet/app/coins/domain/CoinsRemoteDataSource.kt
@@ -1,4 +1,17 @@
 package com.example.cryptowallet.app.coins.domain
 
+import com.example.cryptowallet.app.coins.data.remote.dto.CoinDetailsResponseDto
+import com.example.cryptowallet.app.coins.data.remote.dto.CoinPriceHistoryResponseDto
+import com.example.cryptowallet.app.coins.data.remote.dto.CoinResponseDto
+import com.example.cryptowallet.app.core.domain.DataError
+import com.example.cryptowallet.app.core.domain.Result
+
+
 interface CoinsRemoteDataSource {
+
+    suspend fun getListOfCoins(): Result<CoinResponseDto, DataError.Remote>
+
+    suspend fun getCoinPriceHistory(coinId: String): Result<CoinPriceHistoryResponseDto,DataError.Remote>
+
+    suspend fun getCoinById(coinId: String): Result<CoinDetailsResponseDto, DataError.Remote>
 }
