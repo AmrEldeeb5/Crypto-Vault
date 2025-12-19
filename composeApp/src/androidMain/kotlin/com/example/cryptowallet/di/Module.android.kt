5e43b5d3f3a52d51934da3ea05748ca21c808307
amreldeeb <amreldeeb641@gmail.com>
2025-12-19 04:52:14 +0200

    update HTTP client engine to OkHttp and add Kotlin native compiler daemon setting

--- composeApp/src/androidMain/kotlin/com/example/cryptowallet/di/Module.android.kt
+++ composeApp/src/androidMain/kotlin/com/example/cryptowallet/di/Module.android.kt
@@ -1,11 +1,11 @@
 package com.example.cryptowallet.app.di
 
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.android.Android
+import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.dsl.module
 
 actual val platformModule = module {
 
     // core
-    single<HttpClientEngine> { Android.create() }
+    single<HttpClientEngine> { OkHttp.create() }
 }
