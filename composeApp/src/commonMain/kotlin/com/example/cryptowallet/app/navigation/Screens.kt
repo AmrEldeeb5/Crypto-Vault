14b5ba9dd38adcca6645650ca45caf25b8702099
amreldeeb <amreldeeb641@gmail.com>
2025-12-23 11:42:48 +0200

    change Screens interface to sealed interface for improved type safety

--- composeApp/src/commonMain/kotlin/com/example/cryptowallet/app/navigation/Screens.kt
+++ composeApp/src/commonMain/kotlin/com/example/cryptowallet/app/navigation/Screens.kt
@@ -2,7 +2,7 @@ package com.example.cryptowallet.app.navigation
 
 import kotlinx.serialization.Serializable
 
-interface Screens {
+sealed interface Screens {
 
     @Serializable
     data object Coins : Screens
